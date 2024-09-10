66a96047a73add7d4ebe44a4b5d66981ed46ecb9
Swan Htet Aung <swanhtetaung308@gmail.com>
2024-09-10 12:07:36 +0630

    navbar and reusable modal add

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <template>
-  <div>
+  <div class="flex flex-col min-h-screen font-sans bg-weather-primary">
+    <Navbar />
     <RouterView />
   </div>
 </template>
 
 <script setup>
 import { RouterView } from "vue-router";
+import Navbar from "./components/Navbar.vue";
 </script>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
+
+import "primeicons/primeicons.css";
+
 import "./assets/tailwild.css";
 
 const app = createApp(App);
